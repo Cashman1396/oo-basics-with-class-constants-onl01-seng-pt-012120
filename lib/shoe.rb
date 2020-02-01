67a8e83f9d59d58f85f92339430e3c98064abc0c
Chris Ashman <chrisashman96@gmail.com>
2020-02-01 21:51:32 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,11 +2,11 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  BRAND = []
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
-    BRAND << brand unless BRANDS
+    BRANDS << brand unless BRANDS
   end
 
   def cobble
